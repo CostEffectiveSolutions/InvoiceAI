f0449eb4f7cb44495572bdcfa177f07ae34438ff
Georgi Kirilov <gkkirilov@gmail.com>
2024-03-23 10:37:29 +0200

    Update login

--- components/Header.vue
+++ components/Header.vue
@@ -59,7 +59,7 @@
         <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Company</a> -->
       </PopoverGroup>
       <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span
+        <a href="/login" class="text-sm font-semibold leading-6 text-gray-900">Log in <span
             aria-hidden="true">&rarr;</span></a>
       </div>
     </nav>
