e1c52b0ccee489be5242e31ef2b93e23340b6cd0
Georgi Kirilov <gkkirilov@gmail.com>
2024-08-07 13:04:34 +0300

    Remove redirects

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   css: ['~/assets/main.css'],
   devtools: { enabled: true },
   modules: [
-    // '@nuxtjs/supabase',
+    '@nuxtjs/supabase',
     'nuxt-headlessui',
     // '@nuxtjs/google-fonts',
     '@nuxtjs/tailwindcss',
@@ -16,6 +16,7 @@ export default defineNuxtConfig({
   supabase: {
     url: 'https://lmestjkqmdxeqwmkwrwa.supabase.co',
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtZXN0amtxbWR4ZXF3bWt3cndhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTU0MzIsImV4cCI6MjAyNjM3MTQzMn0.l8RUi4QGwGdti58UAbSobmyUSp9SuPxgRNGKGSVduhs',
+    redirect: false,
     redirectOptions: {
       login: '/',
       callback: '/confirm',
