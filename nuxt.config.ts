ac1a4460080cb2d09155e628ab9899b092fd9b4a
Georgi Kirilov <gkkirilov@gmail.com>
2025-03-01 20:56:28 +0200

    Update supabse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,15 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/supabase', '@nuxtjs/tailwindcss', "@nuxt/fonts", '@nuxt/image', '@nuxt/icon', 'shadcn-nuxt', 'lenis/nuxt', 'v-gsap-nuxt'],
 
-  supabase: {
-    url: 'env',
-    key: 'env',
-    redirect: false,
-    // redirectOptions: {
-    //   login: '/',
-    //   callback: '/confirm',
-    //   include: ['/admin(/*)?']
-    // }
-  },
   shadcn: {
     prefix: '',
     componentDir: './components/ui',
   },
+  supabase: {
+    url: 'https://lmestjkqmdxeqwmkwrwa.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtZXN0amtxbWR4ZXF3bWt3cndhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3OTU0MzIsImV4cCI6MjAyNjM3MTQzMn0.l8RUi4QGwGdti58UAbSobmyUSp9SuPxgRNGKGSVduhs',
+    redirect: false,
+
+  }
+
 })
